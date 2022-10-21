e55a1acf80b7aba5cba1f1df175932887339e473
enimatek-nl <accounts@enimatek.nl>
2022-10-21 15:24:37 +0200

    first

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MudBlazor.Services;
+using JsonClassGenCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices();
+
+var app = builder.Build();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+app.Run();
